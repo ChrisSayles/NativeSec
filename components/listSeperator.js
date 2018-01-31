3f7c8411fdd02285e47647b5cd79721093acc4ef
danielRicaud <ricaud512@gmail.com>
2018-01-31 16:04:58 -0600

    Error list dynamically generated, ordering sometimes inverted

--- components/listSeperator.js
+++ components/listSeperator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListView, StyleSheet } from 'react-native';
+import { ListView, StyleSheet, View } from 'react-native';
 import { Container, Header, Content, List, ListItem, Text, Separator } from 'native-base';
 import data from './customData.json';
 
@@ -15,27 +15,100 @@ export default class ListSeparator extends Component {
 
   render() {
 
-    let highErrors = [];
-    let mediumErrors = [];
-    let lowErrors = [];
+    let highCount = 0;
+    let mediumCount = 0;
+    let lowCount = 0;
 
-    data.riskevents.map(riskevent => {
-      if(riskevent.appinstance === riskevent && 
-         riskevent.priority === "High"){
-          highErrors.push(riskevent);  
+
+
+
+    
+
+
+    const renderErrors = () => data.riskevents.map(riskevent => {
+      if(riskevent.appinstance === this.selectedInstance && 
+         riskevent.priority === "High" && highCount < 1){
+          //highErrors.push(riskevent);  
+          highCount++;
+          return (
+            <View>
+              <Separator bordered style={styles.swipeHeader}>
+                <Text style={styles.swipeHeaderText}>HIGH</Text>
+              </Separator>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
       }
-      else if(riskevent.appinstance === riskevent && 
-              riskevent.priority === "Medium"){
-          mediumErrors.push(riskevent);
+      else if(riskevent.appinstance === this.selectedInstance && 
+              riskevent.priority === "High" && highCount > 0){
+          //mediumErrors.push(riskevent);
+        highCount++;
+          return (
+            <View>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
       }
-      else if(riskevent.appinstance === riskevent && 
-              riskevent.priority === "Low"){
-          lowErrors.push(riskevent);
+      else if(riskevent.appinstance === this.selectedInstance && 
+         riskevent.priority === "Medium" && highCount < 1){
+          //highErrors.push(riskevent);  
+          mediumCount++;
+          return (
+            <View>
+              <Separator bordered style={styles.swipeHeader}>
+                <Text style={styles.swipeHeaderText}>MEDIUM</Text>
+              </Separator>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
       }
+      else if(riskevent.appinstance === this.selectedInstance && 
+              riskevent.priority === "Medium" && highCount > 0){
+          //mediumErrors.push(riskevent);
+        mediumCount++;
+          return (
+            <View>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
+      }
+      else if(riskevent.appinstance === this.selectedInstance && 
+         riskevent.priority === "Low" && highCount < 1){
+          //highErrors.push(riskevent);  
+          lowCount++;
+          return (
+            <View>
+              <Separator bordered style={styles.swipeHeader}>
+                <Text style={styles.swipeHeaderText}>LOW</Text>
+              </Separator>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
+      }
+      else if(riskevent.appinstance === this.selectedInstance && 
+              riskevent.priority === "Low" && highCount > 0){
+          //mediumErrors.push(riskevent);
+        lowCount++;
+          return (
+            <View>
+              <ListItem >
+                <Text >{riskevent.title}</Text>
+              </ListItem>
+            </View>
+          );
+      }
+      
     });
-
-
-    const renderErrors = (instance) => 
 
 
 
@@ -45,37 +118,46 @@ export default class ListSeparator extends Component {
     return (
       <Container style={styles.container}>
         <Content>
-          <Separator bordered style={styles.swipeHeader}>
-            <Text style={styles.swipeHeaderText}>HIGH</Text>
-          </Separator>
-          <ListItem >
-            <Text >DDOS</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Admin Changes</Text>
-          </ListItem>
-          <ListItem last>
-            <Text>Multiple IP's</Text>
-          </ListItem>
-          <Separator bordered style={styles.swipeHeader}>
-            <Text style={styles.swipeHeaderText}>MEDIUM</Text>
-          </Separator>
-          <ListItem>
-            <Text>Copied Folder</Text>
-          </ListItem>
-          <Separator bordered style={styles.swipeHeader}>
-            <Text style={styles.swipeHeaderText}>LOW</Text>
-          </Separator>
-          <ListItem last>
-            <Text>Invalid Password</Text>
-          </ListItem>
-          <ListItem last>
-            <Text>Changed Password</Text>
-          </ListItem>
-
+          {renderErrors()}
         </Content>
       </Container>
     );
+
+
+    // return (
+    //   <Container style={styles.container}>
+    //     <Content>
+    //       <Separator bordered style={styles.swipeHeader}>
+    //         <Text style={styles.swipeHeaderText}>HIGH</Text>
+    //       </Separator>
+    //       <ListItem >
+    //         <Text >DDOS</Text>
+    //       </ListItem>
+    //       <ListItem>
+    //         <Text>Admin Changes</Text>
+    //       </ListItem>
+    //       <ListItem last>
+    //         <Text>Multiple IP's</Text>
+    //       </ListItem>
+    //       <Separator bordered style={styles.swipeHeader}>
+    //         <Text style={styles.swipeHeaderText}>MEDIUM</Text>
+    //       </Separator>
+    //       <ListItem>
+    //         <Text>Copied Folder</Text>
+    //       </ListItem>
+    //       <Separator bordered style={styles.swipeHeader}>
+    //         <Text style={styles.swipeHeaderText}>LOW</Text>
+    //       </Separator>
+    //       <ListItem last>
+    //         <Text>Invalid Password</Text>
+    //       </ListItem>
+    //       <ListItem last>
+    //         <Text>Changed Password</Text>
+    //       </ListItem>
+
+    //     </Content>
+    //   </Container>
+    // );
   }
 }
 
